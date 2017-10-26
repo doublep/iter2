723daab80b7a44861801a9399c368ec696f3ae8a
Paul Pogonyshev <pogonyshev@gmail.com>
2017-10-26 23:02:57 +0200

    Use Travis CI for automated testing.

---
+++ run-tests.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+set -e
+
+if [ -z "$EMACS" ] ; then
+    EMACS=emacs
+fi
+
+if [ -z "$ERT_SELECTOR" ] ; then
+    ERT_SELECTOR=t
+fi
+
+$EMACS -batch                                                           \
+       --eval "(message \"Using Emacs %s\" (emacs-version))"            \
+       -l iter2.el                                                      \
+       -l test/iter2-test.el                                            \
+       --eval "(ert-run-tests-batch-and-exit (quote ${ERT_SELECTOR}))"
+
+$EMACS -Q --batch                                                       \
+       --eval "(setq byte-compile-error-on-warn t)"                     \
+       --eval "(batch-byte-compile)" iter2.el
