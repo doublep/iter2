a6e228eefed8dd6a405c235c15e7a4ea726cae02
Paul Pogonyshev <pogonyshev@gmail.com>
2020-05-10 15:55:02 +0200

    Bump version.

--- iter2.el
+++ iter2.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.9.10
+;; Version:    0.9.11
 ;; Keywords:   elisp, extensions
 ;; Homepage:   https://github.com/doublep/iter2
 ;; Package-Requires: ((emacs "25.1"))
