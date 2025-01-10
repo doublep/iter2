8b3fb215d22161f303ecd6c96c0693b96bda3c4c
Paul Pogonyshev <pogonyshev@gmail.com>
2025-01-10 21:05:10 +0100

    Release iter2 2.0

--- iter2.el
+++ iter2.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    1.5snapshot
+;; Version:    2.0
 ;; Keywords:   elisp, extensions
 ;; Homepage:   https://github.com/doublep/iter2
 ;; Package-Requires: ((emacs "25.1"))
